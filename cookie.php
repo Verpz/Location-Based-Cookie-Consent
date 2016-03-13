9d375b45e2f1d8248ea8335ef618674ec016c57d
Verpz <lachlan186@gmail.com>
2016-03-13 11:48:47 +1100

    New API with SSL and Fixed IP Get Func

--- cookie.php
+++ cookie.php
@@ -1,24 +1,28 @@
 <?php
 /* Made by Verpz https://github.com/Verpz/ Licensed Under Apache License 2.0 Details On GitHub */
 function GetIP()
-{
-    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key)
-    {
-        if (array_key_exists($key, $_SERVER) === true)
-        {
-            foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
-            {
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
-                {
-                    print $ip;
-                }
-            }
-        }
-    }
-}
+ {
+		//GetIP function by ChrisWiegman
+		if ( function_exists( 'apache_request_headers' ) ) {
+			$headers = apache_request_headers();
+		} else {
+			$headers = $_SERVER;
+		}
+		
+		if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+			$the_ip = $headers['X-Forwarded-For'];
+		} elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )
+		) {
+			$the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+		} else {
+
+			$the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+		}
+		return $the_ip;
+	}
 
 $address = GetIP();
-$xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=$address");
-echo $xml->geoplugin_countryName ;
+$xml = simplexml_load_file("https://ip-json.rhcloud.com/xml/$address");
+echo $xml->country_name;
 
 ?>
