4e47f0960afce0d2b397384da6b1403a814d490d
Verpz <lachlan186@gmail.com>
2016-03-13 15:44:28 +1100

    Added EU Country List and If Statement

--- cookie.php
+++ cookie.php
@@ -2,13 +2,13 @@
 /* Made by Verpz https://github.com/Verpz/ Licensed Under Apache License 2.0 Details On GitHub */
 function GetIP()
  {
-		//GetIP function by ChrisWiegman
+		//GetIP function by Chris Wiegman
 		if ( function_exists( 'apache_request_headers' ) ) {
 			$headers = apache_request_headers();
 		} else {
 			$headers = $_SERVER;
 		}
-		
+
 		if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
 			$the_ip = $headers['X-Forwarded-For'];
 		} elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )
@@ -23,6 +23,13 @@ function GetIP()
 
 $address = GetIP();
 $xml = simplexml_load_file("https://ip-json.rhcloud.com/xml/$address");
-echo $xml->country_name;
+$country = $xml->country_name;
+$EU = array("Austria", "Belgium", "Bulgaria", "Croatia", "Republic of Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden", "United Kingdom");
+
+if (in_array($country, $EU)) {
+  echo "Currently In $country";
+} else {
+  echo "Not in Europe";
+}
 
 ?>
