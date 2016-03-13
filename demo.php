dbd6c5135d7903df54c38ad8997d335e904914dc
Verpz <lachlan186@gmail.com>
2016-03-13 21:10:27 +1100

    Demo Completed

--- demo.php
+++ demo.php
@@ -5,7 +5,9 @@
 <html>
   <head>
     <title>This Is A Demo</title>
+
   </head>
   <body>
+    <h2>If your IP address is located in a Euro Country, the popup will show.</h2><br /><h2> To see what the popup looks like normally, refer to popup-demo.html</h2>
   </body>
 </html>
