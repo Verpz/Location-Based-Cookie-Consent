b6e30f05e8074dd4533db405da33e5f6bce01664
Verpz <lachlan186@gmail.com>
2016-05-31 20:21:41 +1000

    Add More Information

--- demo.php
+++ demo.php
@@ -1,7 +1,5 @@
 <!-- Made by Verpz https://github.com/Verpz/ Licensed Under MIT License Details On GitHub -->
-<?php
-  require("cookie.php");
- ?>
+
 <html>
   <head>
     <title>This Is A Demo</title>
@@ -10,4 +8,9 @@
   <body>
     <h2>If your IP address is located in a Euro Country, the popup will show.</h2><br /><h2> To see what the popup looks like normally, refer to popup-demo.html</h2>
   </body>
+  <?php
+    require("cookie.php");
+    echo 'Current country: ' . $country . '<br />';
+    echo 'IP: ' . GetIP();
+   ?>
 </html>
